9ca58a789138f777eae38277657644ef84dfc404
le0-n <farrux.farxodov2000@gmail.com>
2023-02-16 12:48:55 +0500

    v1.0

---
+++ main.js
@@ -0,0 +1,10 @@
+let x = +prompt(`Bo'linuvchini kiriting: `)
+let y = +prompt(`Bo'luvchini kiriting: `)
+
+while (x > 0) {
+    x -= 1
+    if (x % y == 0) {
+        let z = x
+        alert(z)
+    }
+}
